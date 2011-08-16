b67e6fc0dbbe60949970e04664d87a531cb388b9
snoop1q <snoop3rson@gmail.com>
2011-08-16 22:54:21 +0430

    mosq killer ver 0.02

--- MosquitoKiller/gen/mosquito/killer/R.java
+++ MosquitoKiller/gen/mosquito/killer/R.java
@@ -17,9 +17,8 @@ public final class R {
         public static final int mosq=0x7f020003;
     }
     public static final class id {
-        public static final int about=0x7f060003;
+        public static final int about=0x7f060002;
         public static final int pause=0x7f060001;
-        public static final int restart=0x7f060002;
         public static final int start=0x7f060000;
     }
     public static final class layout {
@@ -30,10 +29,9 @@ public final class R {
         public static final int music=0x7f040000;
     }
     public static final class string {
-        public static final int about=0x7f050003;
-        public static final int app_name=0x7f050004;
+        public static final int about=0x7f050002;
+        public static final int app_name=0x7f050003;
         public static final int pause=0x7f050001;
-        public static final int restart=0x7f050002;
         public static final int start=0x7f050000;
     }
 }
